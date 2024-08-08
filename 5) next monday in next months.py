cb581fcecdb1dc1bb7faf1316bd84e1bf86aca31
askarthikeyan-007 <karthikeyana5143.sse@saveetha.com>
2024-08-08 08:22:03 +0530

    Delete 5) next monday in next months.py

--- 5) next monday in next months.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, timedelta
-today = datetime.now()
-first_day_of_next_month = datetime(today.year + (today.month // 12), ((today.month % 12) + 1), 1)
-days_until_monday = (7 - first_day_of_next_month.weekday()) % 7
-first_monday_of_next_month = first_day_of_next_month + timedelta(days=days_until_monday)
-print(first_monday_of_next_month)
