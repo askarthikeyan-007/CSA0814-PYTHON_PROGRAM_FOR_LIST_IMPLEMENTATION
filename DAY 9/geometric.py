faa5b6f3c786388e6ee27a35bb33aa2daf2b52ab
askarthikeyan-007 <karthikeyana5143.sse@saveetha.com>
2024-08-22 09:44:17 +0530

    Update geometric.py

--- DAY 9/geometric.py
+++ DAY 9/geometric.py
@@ -1,12 +1,4 @@
-sequence = [2,6,18,54]
-a=sequence[1]/sequence[0]
-flag=0
-for i in range(1,len(sequence)-1):
-    if sequence[i+1]/sequence[i]!=a:
-        flag=1
-        break
-if flag==0:
-    print("It is a Geometric Progression")
-else:
-    print("It is not a Geometric Progression")
-        
+def geometric_progression(a, r, n):
+    return [a * r**i for i in range(n)]
+gp = geometric_progression(2, 3, 5)  
+print(gp)  
