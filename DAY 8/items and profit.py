19a7b67aba7eaaf3909bb78c246ede32399c6fa2
askarthikeyan-007 <karthikeyana5143.sse@saveetha.com>
2024-08-22 10:03:17 +0530

    Update items and profit.py

--- DAY 8/items and profit.py
+++ DAY 8/items and profit.py
@@ -1,5 +1,4 @@
 items = [{'name': 'Item1', 'cost': 20, 'selling': 30}, 
          {'name': 'Item2', 'cost': 15, 'selling': 25}]
-
 profits = {item['name']: item['selling'] - item['cost'] for item in items}
 print(profits)
