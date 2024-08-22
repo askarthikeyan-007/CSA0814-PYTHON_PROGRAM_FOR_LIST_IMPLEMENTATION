f6fdc97ecf4481b1eb5125a4096725410685d15d
askarthikeyan-007 <karthikeyana5143.sse@saveetha.com>
2024-08-22 10:05:35 +0530

    Update error occurence.py

--- DAY 9/error occurence.py
+++ DAY 9/error occurence.py
@@ -1,11 +1,9 @@
 try:
     num1 = int(input("Enter the first four-digit number: "))
     num2 = int(input("Enter the second four-digit number: "))
-    
     if not (1000 <= num1 <= 9999) or not (1000 <= num2 <= 9999):
         print("Both numbers must be four-digit numbers.")
-    else:
-        
+    else:   
         mul = num1 * num2
         r = str(mul)
         if r == r[::-1]:
